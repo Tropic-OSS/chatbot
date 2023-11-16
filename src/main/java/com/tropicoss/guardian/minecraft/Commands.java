0b75f814fc0b2ebf46b80c387ca1190df32f38fd
Tropic <jordan.bmp.mckoy@gmail.com>
2023-11-15 19:42:24 -0500

    Formatted Code and Added Avatar

--- src/main/java/com/tropicoss/guardian/minecraft/Commands.java
+++ src/main/java/com/tropicoss/guardian/minecraft/Commands.java
@@ -1,7 +1,9 @@
 package com.tropicoss.guardian.minecraft;
 
+import static com.tropicoss.guardian.Guardian.SOCKET_CLIENT;
 import static net.minecraft.server.command.CommandManager.literal;
 
+import com.tropicoss.guardian.config.Config;
 import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
 import net.minecraft.text.Text;
 
@@ -15,6 +17,16 @@ public class Commands {
                         context -> {
                           context.getSource().sendFeedback(() -> Text.literal("Hello"), false);
                           return 1;
-                        })));
+                        })
+                    .then(
+                        literal("reload")
+                            .executes(
+                                context -> {
+                                  context
+                                      .getSource()
+                                      .sendFeedback(() -> Text.literal("Reloading"), false);
+                                  SOCKET_CLIENT.reload();
+                                  return 1;
+                                }))));
   }
 }
